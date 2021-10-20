3b851ab65806bf8b916b12db17d5348f2261562f
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-20 23:54:07 +0900

    [追加]Calculatorコンポーネント作成

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,97 @@
+import { useCalcurator } from '@/hooks/useCalculator'
+import { css } from '@emotion/react'
+
+import type { Operators } from 'src/types'
+
+const arr = [...Array(9)].map((_, index: number) => index + 1)
+const symbols: Operators[] = ['+', '-', '*', '/']
+
+export const Calculator: React.VFC = () => {
+  const {
+    firstVal,
+    secondVal,
+    result,
+    operator,
+    setValue,
+    handleOperator,
+    calculate,
+    clear,
+  } = useCalcurator()
+
+  return (
+    <div>
+      <div css={display}>
+        {result ? result : `${firstVal} ${operator} ${secondVal}`}
+      </div>
+      <div css={flex}>
+        <div css={grid}>
+          {arr.map((number) => (
+            <button key={number} css={button} onClick={() => setValue(number)}>
+              {number}
+            </button>
+          ))}
+          <button css={button} onClick={clear}>
+            C
+          </button>
+
+          <button css={button} onClick={() => setValue(0)}>
+            0
+          </button>
+          <button css={button} onClick={calculate}>
+            =
+          </button>
+        </div>
+        <div css={column}>
+          {symbols.map((sym) => (
+            <button key={sym} css={button} onClick={() => handleOperator(sym)}>
+              {sym}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const display = css`
+  padding: 0 0.2em;
+  border: 1px solid #ccc;
+  margin-bottom: 20px;
+  font-size: 5rem;
+  text-align: right;
+`
+
+const flex = css`
+  display: flex;
+  gap: 20px;
+`
+
+const grid = css`
+  display: grid;
+  height: 200px;
+  flex: 3;
+  justify-content: end;
+  gap: 20px;
+  grid-template-columns: repeat(3, 1fr);
+`
+
+const button = css`
+  height: 80px;
+  background-color: #ccc;
+  font-size: 3rem;
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  &:active {
+    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.16) inset;
+  }
+`
+
+const column = css`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 20px;
+`
